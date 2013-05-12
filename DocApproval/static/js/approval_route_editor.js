99702f5c5bc432f32c34b279ee2ffa73c21f2cf1
john <_john_@bk.ru>
2013-05-12 12:56:48 -0400

    Switched back to tanle layout in order to provide fluid layout for dropdowns

--- DocApproval/static/js/approval_route_editor.js
+++ DocApproval/static/js/approval_route_editor.js
@@ -5,10 +5,20 @@
     var logger = console ? console.log : alert;
     var ui_notifier = logger;
 
+    var wrapper_element = "table";
+    var row_element = "tr";
+    var cell_element = "td";
+    var row_celector = "> tbody > " + row_element;
+    var cell_celector = "> tbody > " + row_element + " > " + cell_element;
+
     var row_button_config = {
         'add': {image: "add.png", cssClass: 'btn add_button'},
         'remove': {image: "remove.png", cssClass: 'btn remove_button'}
     };
+
+    function wrap(elem) {
+        return "<"+elem+"></"+elem+">";
+    }
 
     function download_approver_list(target_url) {
         return $.ajax({
@@ -23,27 +33,29 @@
         var row_count = 0;
 
         function make_wrapper() {
-            return $("<div></div>").addClass("editor-wrapper");
+            return $(wrap(wrapper_element)).addClass("editor-wrapper table table-striped");
         }
 
         function make_row(row_data) {
-            var row = $("<div></div>").attr({'id': "row_" + row_count}).addClass("row-fluid");
+            var row = $(wrap(row_element)).attr({'id': "row_" + row_count}).addClass("row");
             make_buttons_cell(row).appendTo(row);
-            $("<span></span>").addClass("step-label span2").text("Шаг №" + row_count).appendTo(row);
-            logger(row_data);
-            for (var i=0; i<row_data.length; i++) {
+            $(wrap(cell_element)).addClass("step-label").text("Шаг №" + row_count).appendTo(row);
+            var steps_cell = $(wrap(cell_element)).addClass("steps").appendTo(row);
+            var elements_wrapper = $("<div></div>").attr({'id': "row_wrapper_" + row_count});
+            elements_wrapper.addClass("row-fluid").appendTo(steps_cell);
+            for (var i = 0; i < row_data.length; i++) {
                 var dropdown = make_dropdown(that.approvers, row_data, row_data[i]);
-                $("<span></span>").addClass("span3").appendTo(row).append(dropdown);
+                $("<span></span>").addClass("span3").appendTo(elements_wrapper).append(dropdown);
             }
             return row;
         }
 
-        function make_dropdown(approvers, approvers_in_row, selected_approver){
+        function make_dropdown(approvers, approvers_in_row, selected_approver) {
             var select = $("<select></select>").addClass("input-medium");
             $("<option></option>").text("-----").appendTo(select);
-            for (var k in approvers){
+            for (var k in approvers) {
                 if (!approvers.hasOwnProperty(k) ||
-                    ($.inArray(k, approvers_in_row) != -1 && k !=selected_approver))
+                    ($.inArray(k, approvers_in_row) != -1 && k != selected_approver))
                     continue;
                 var approver = approvers[k];
                 var option = $("<option></option>").attr("value", k).text(approver.name);
@@ -54,33 +66,37 @@
         }
 
         function make_buttons_cell(row) {
-            var cell = $("<span></span>").addClass("span2 buttons-cell");
-            make_button(row_button_config['add'], function(){ that.add_row({},row);}).appendTo(cell);
-            make_button(row_button_config['remove'], function(){ that.delete_row(row);}).appendTo(cell);
+            var cell = $(wrap(cell_element)).addClass("span2 buttons-cell");
+            make_button(row_button_config['add'],function () {
+                that.add_row({}, row);
+            }).appendTo(cell);
+            make_button(row_button_config['remove'],function () {
+                that.delete_row(row);
+            }).appendTo(cell);
             return cell;
         }
 
-        function make_button(button_cfg, click_handler){
+        function make_button(button_cfg, click_handler) {
             var btn = $("<button></button>").addClass(button_cfg.cssClass);
             $("<img/>").attr("src", globals.static_root + "/img/icons/" + button_cfg.image).appendTo(btn);
             btn.click(click_handler);
             return btn;
         }
 
-        function relabel_rows(){
-            var rows = that.editor_wrapper.find("> div");
-            for (var i=0; i<rows.length; i++){
+        function relabel_rows() {
+            var rows = that.editor_wrapper.find(row_celector);
+            for (var i = 0; i < rows.length; i++) {
                 var row = rows[i];
-                $(row).children("span.step-label").text("Шаг №"+(i+1));
-                $(row).attr('id', "row_"+(i+1));
+                $(row).children(cell_element + ".step-label").text("Шаг №" + (i + 1));
+                $(row).attr('id', "row_" + (i + 1));
             }
         }
 
         function toggle_delete_buttons(enabled) {
             if (enabled)
-                that.editor_wrapper.find("> div > span > .remove_button").removeAttr('disabled');
+                that.editor_wrapper.find(cell_celector + "> .remove_button").removeAttr('disabled');
             else
-                that.editor_wrapper.find("> div > span > .remove_button").attr({disabled: 'disabled'});
+                that.editor_wrapper.find(cell_celector + "> .remove_button").attr({disabled: 'disabled'});
         }
 
 
@@ -88,7 +104,7 @@
         this.set_data = function (data) {
             that.data = data;
         };
-        this.set_approvers = function(approvers){
+        this.set_approvers = function (approvers) {
             that.approvers = approvers;
         }
         this.render = function () {
@@ -99,7 +115,7 @@
                 that.add_row(that.data[k]);
             }
         };
-        this.add_row = function(data, prev_row) {
+        this.add_row = function (data, prev_row) {
             var new_row = make_row(data);
             row_count += 1;
             if (prev_row)
@@ -109,7 +125,7 @@
             relabel_rows();
             toggle_delete_buttons(row_count != 1);
         };
-        this.delete_row = function(row) {
+        this.delete_row = function (row) {
             row.remove();
             row_count -= 1;
             relabel_rows();
