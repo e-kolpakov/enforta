093f9a2a08e08f1c9482207da501d4daf033b11f
john <_john_@bk.ru>
2013-05-25 13:08:39 -0400

    More verbose logging for development setup

--- portal/settings/development.py
+++ portal/settings/development.py
@@ -1,3 +1,5 @@
+import os
+
 ADMINS = ()
 
 DEBUG = True
@@ -10,5 +12,58 @@ DATABASES = {
         'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '',
+    }
+}
+
+LOGGING_DIRECTORY = "/var/log"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(module)s [%(levelname)s] %(process)d %(thread)d %(message)s'
+        },
+        'generic': {
+            'format': '%(asctime)s %(name)s->%(funcName)s:%(lineno)d [%(levelname)s]: %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGGING_DIRECTORY, 'doc-approval/django.log'),
+            'maxBytes': 102400,
+            'backupCount': 1,
+            'formatter': 'generic',
+            'encoding': 'UTF-8'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': False
+        },
+        'DocApproval': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'file'],
+            'level': 'ERROR',
+            'propagate': True,
+        }
     }
 }
