0a85a780bd9026af20888ad72d2b3d7360709756
john <_john_@bk.ru>
2013-05-26 13:24:07 -0400

    Added status transitions project => negotiation, begotiation => project

--- DocApproval/request_management/status_management.py
+++ DocApproval/request_management/status_management.py
@@ -1,12 +1,71 @@
 import logging
 
+from django.db import transaction
+from ..models.approval import ApprovalProcess
+from ..models.request import RequestStatus
+
+
+class RequestStatusTransitionBase(object):
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super(RequestStatusTransitionBase, self).__init__(*args, **kwargs)
+
+    def apply(self):
+        raise NotImplementedError("Must be overridden in child classes")
+
+
+class ProjectToNegotiationTransition(RequestStatusTransitionBase):
+    _logger = logging.getLogger(__name__ + ".ProjectToNegotiationTransition")
+
+    @transaction.commit_on_success
+    def apply(self):
+        self._logger.debug("Applying project => negotiation transition")
+        try:
+            last_attempt = self.request.approval_route.processes.get(is_current=True)
+            last_attempt.is_current = False
+            last_attempt.save()
+            last_attempt_number = last_attempt.attempt_number
+        except ApprovalProcess.DoesNotExist:
+            last_attempt_number = 0
+
+        new_attempt = ApprovalProcess.objects.create(
+            route=self.request.approval_route,
+            attempt_number=last_attempt_number + 1,
+            is_current=True
+        )
+        return new_attempt
+
+
+class NegotiationToProjectTransition(RequestStatusTransitionBase):
+    _logger = logging.getLogger(__name__ + ".NegotiationToProjectTransition")
+
+    def apply(self):
+        self._logger.debug("Applying negotiation => project transition")
+        try:
+            last_attempt = self.request.approval_route.processes.get(is_current=True)
+            last_attempt.is_current = False
+            last_attempt.save()
+        except ApprovalProcess.DoesNotExist:
+            pass
+
 
 class RequestStatusManager(object):
     _logger = logging.getLogger(__name__ + ".RequestStatusManager")
 
-    def __init__(self, instance):
-        self._instance = instance
+    _valid_transitions = {
+        (RequestStatus.PROJECT, RequestStatus.NEGOTIATION): ProjectToNegotiationTransition,
+        (RequestStatus.NEGOTIATION, RequestStatus.PROJECT): NegotiationToProjectTransition
+    }
+
+    def __init__(self, request_instance):
+        self._request_instance = request_instance
 
     def handle_status_update(self, old_status, new_status):
         self._logger.debug(
-            u"Handling status change on instance {0} - {1} => {2}".format(self._instance, old_status, new_status))
+            u"Handling status change on instance {0} - {1} => {2}".format(self._request_instance, old_status,
+                                                                          new_status))
+
+        if (old_status.code, new_status.code) in self._valid_transitions:
+            transition_class = self._valid_transitions[(old_status.code, new_status.code)]
+            transition = transition_class(self._request_instance)
+            transition.apply()
