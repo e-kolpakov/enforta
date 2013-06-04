3c7ff0971678bfaa23e19ab92c5643555c624757
john <_john_@bk.ru>
2013-06-03 20:05:55 -0400

    Fixes for bugs revealed during deployment

---
+++ DocApproval/static/js/pages/edit_form.js
@@ -0,0 +1,17 @@
+/*global define, page_data*/
+define(['jquery', 'app/forms'], function ($) {
+    function get_page_data() {
+        return page_data; // assuming global page-specific page_data variable
+    }
+
+    var config = get_page_data();
+
+    return function () {
+        $(function () {
+            $(config.target).error_handling_form({
+                error_popup_selector: config.error_popup_selector,
+                field_errors_selector: config.field_errors_selector
+            });
+        });
+    };
+});
