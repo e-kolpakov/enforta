03c023cec189c555648313528e6de3417eae4e57
john <_john_@bk.ru>
2013-06-06 22:26:20 -0400

    Updated requirements.txt and install-packages with weasy-related stuff

--- deployment/staging/0-install_packages.sh
+++ deployment/staging/0-install_packages.sh
@@ -3,3 +3,4 @@
 sudo apt-get -y install apache2 libapache2-mod-wsgi
 sudo apt-get -y install postgresql postgresql-client postgresql-server-dev-9.1
 sudo apt-get -y install python-pip python2.7-dev
+sudo apt-get -y install libffi-dev libxml2-dev libxslt-dev
