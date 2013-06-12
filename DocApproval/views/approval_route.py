a59349b356a44d71c37aff6aec92ed5654448dfc
john <_john_@bk.ru>
2013-06-11 21:33:42 -0400

    Fix for error when retrieving all approvers

--- DocApproval/views/approval_route.py
+++ DocApproval/views/approval_route.py
@@ -14,7 +14,7 @@ from django.db.models import Q
 from django.http import HttpResponse
 
 from ..menu import MenuModifierViewMixin, RequestContextMenuManagerExtension
-from ..models import ApprovalRoute, Permissions, ApprovalRouteExceptionBase
+from ..models import ApprovalRoute, Permissions, ApprovalRouteExceptionBase, UserProfile
 from ..messages import ApprovalRouteMessages
 from ..url_naming.names import ApprovalRoute as ApprovalRouteUrls
 from ..utilities.utility import get_url_base
@@ -156,8 +156,8 @@ class ApproversListJson(View):
     def _get_all_approvers(self):
         perm = Permission.objects.get(codename=Permissions.Request.CAN_APPROVE_REQUESTS)
         users = User.objects.filter(Q(groups__permissions=perm) | Q(user_permissions=perm)).distinct()
-        users.select_related('profile')
-        return (user.profile for user in users)
+        profiles = UserProfile.objects.filter(user__in=users)
+        return profiles
 
     def post(self, request, *args, **kwargs):
         data = {
