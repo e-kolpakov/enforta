bfbc76b4160d167ce6b2f5e04dcb135e18f1b769
john <_john_@bk.ru>
2013-06-11 23:11:34 -0400

    Commented out debug print of environment settings. Added robots.txt and favicon

--- portal/settings/__init__.py
+++ portal/settings/__init__.py
@@ -9,7 +9,7 @@ local_settings_file = os.environ.get('EnvironmentType', 'development')
 if 'migrate' in sys.argv or 'syncdb' in sys.argv:
     local_settings_file = 'database_migration'
 
-sys.stderr.write("Importing local settings from {0}\n".format(local_settings_file))
+# sys.stderr.write("Importing local settings from {0}\n".format(local_settings_file))
 
 try:
     _imported = __import__(local_settings_file, globals(), locals(), [], -1)
@@ -18,3 +18,5 @@ try:
             locals()[setting] = getattr(_imported, setting)
 except ImportError:
     pass
+
+    # sys.stderr.write("DEBUG is {0}\n".format(locals()['DEBUG']))
