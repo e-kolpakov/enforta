202f4b472ec324a555172a9a4d6b8bc8b79eaba8
john <_john_@bk.ru>
2013-06-16 19:05:01 -0400

    My approvals home page list

--- DocApproval/views/common.py
+++ DocApproval/views/common.py
@@ -6,6 +6,7 @@ from DocApproval.models import Groups, Request
 class HomePagePartBase(object):
     show_to_groups = ()
     template = None
+    id = None
 
     def should_show(self, user, user_groups):
         return set(user_groups) & set(self.show_to_groups)
@@ -17,6 +18,7 @@ class HomePagePartBase(object):
 class MyRequestsHomePagePart(HomePagePartBase):
     show_to_groups = (Groups.USERS, )
     template = "home_page/my_approvals.html"
+    id = 'my_approvals'
 
     def get_template_data(self, request):
         return Request.objects.get_accessible_requests(request.user)
@@ -31,7 +33,7 @@ class HomePage(TemplateView):
         for part_class in self._parts:
             part = part_class()
             if part.should_show(request.user, groups):
-                result.append((part.template, part.get_template_data(request)))
+                result.append((part.template, part.get_template_data(request), part.id))
         return result
 
     def get(self, request, *args, **kwargs):
