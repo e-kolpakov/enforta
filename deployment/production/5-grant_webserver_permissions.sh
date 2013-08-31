68d641d5b4f7c322665a7c5589bff7d1d2bc1fc2
john <_john_@bk.ru>
2013-08-31 14:28:46 +0400

    Fixes for logging

--- deployment/production/5-grant_webserver_permissions.sh
+++ deployment/production/5-grant_webserver_permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$LOG_PATH=/home/enfortit/docapproval/log
-$UPLOAD_PATH=/var/uploads/doc-approval
-sudo mkdir $LOG_PATH && sudo chown -R john:www-data $LOG_PATH && sudo chmod g+s -R $LOG_PATH
-sudo mkdir -p $UPLOAD_PATH && sudo chown www-data $UPLOAD_PATH && sudo chmod 644 $UPLOAD_PATH
+LOG_PATH=/home/enfortit/docapproval/log
+UPLOAD_PATH=/var/uploads/doc-approval
+sudo mkdir $LOG_PATH && sudo chown -R enfortit:www-data $LOG_PATH && sudo chmod g+ws -R $LOG_PATH
+sudo mkdir -p $UPLOAD_PATH && sudo chown www-data $UPLOAD_PATH && sudo chmod 775 $UPLOAD_PATH
--- deployment/staging/5-grant_webserver_permissions.sh
+++ deployment/staging/5-grant_webserver_permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LOG_PATH=/home/john/enforta/log
 UPLOAD_PATH=/var/uploads/doc-approval
-sudo mkdir $LOG_PATH && sudo chown -R john:www-data $LOG_PATH && sudo chmod g+s -R $LOG_PATH
-sudo mkdir -p $UPLOAD_PATH && sudo chown www-data $UPLOAD_PATH && sudo chmod 644 $UPLOAD_PATH
+sudo mkdir $LOG_PATH && sudo chown -R john:www-data $LOG_PATH && sudo chmod g+ws -R $LOG_PATH
+sudo mkdir -p $UPLOAD_PATH && sudo chown john:www-data $UPLOAD_PATH && sudo chmod 775 $UPLOAD_PATH
