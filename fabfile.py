4f3c1326654299051b227c0427b0666db0dfd899
john <_john_@bk.ru>
2013-08-31 13:48:39 +0400

    Fixed some tests

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,12 @@
-from fabric.api import local
+from fabric.api import local, settings, abort, cd, run
+from fabric.contrib.console import confirm
 
 
 def test():
-    local("./manage.py test DocApproval")
+    with settings(warn_only=True):
+        result = local("./manage.py test DocApproval")
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
 
 
 def commit():
@@ -14,6 +18,19 @@ def push():
 
 
 def prepare_deploy():
-    test()
-    commit()
-    push()
+    """prepares code to be deployed"""
+    # test()
+    # commit()
+    # push()
+
+
+def deploy():
+    """deploys code"""
+    code_dir = '/home/john/enforta/'
+    code_dir = '/home/enfortit/enforta/'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone user@vcshost:/path/to/repo/.git %s" % code_dir)
+    with cd(code_dir):
+        run("git pull")
+        run("touch app.wsgi")
