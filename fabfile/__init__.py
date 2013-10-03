40c0e2aacca9f21a165594e8ba4de91e1e0e0f26
john <_john_@bk.ru>
2013-10-03 11:26:52 +0400

    Provisioning scripts

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,11 +1,41 @@
+from fabric.decorators import task
 from fabric.state import env
 
 __author__ = 'john'
+git_repo = 'https://e_kolpakov@bitbucket.org/e_kolpakov/enforta.git'
 
-env.roledefs = {
-    'production': ['enfortit@87.241.226.36'],
-    'staging': ['enfortit@87.241.226.36']
-}
+
+class Environments:
+    class Production:
+        VENV = "DocApproval"
+        SITE_ROOT = '/home/enfortit/docapproval/'
+
+    class Staging:
+        VENV = "DocApprovalStaging"
+        SITE_ROOT = '/home/enfortit/docapproval-staging/'
+
+    class Development:
+        VENV = "DocApproval"
+        SITE_ROOT = '/home/john/GitRoot/Enforta/enforta/'
+
+
+@task
+def production():
+    env.hosts = ['enfortit@87.241.226.36']
+    env.environment = Environments.Production
+
+
+@task
+def staging():
+    env.hosts = ['enfortit@87.241.226.36']
+    env.environment = Environments.Staging
+
+
+@task
+def development():
+    env.hosts = ['localhost']
+    env.environment = Environments.Development
+
 
 import db
 import deploy
--- fabfile/provision.py
+++ fabfile/provision.py
@@ -1,11 +1,42 @@
+from contextlib import contextmanager
+from fabric.context_managers import prefix, shell_env
 from fabric.decorators import task
+from fabric.operations import sudo, run
+from fabric.state import env
 
 
-@task
+@contextmanager
+def virtualenv(environment_name):
+    with prefix("source /usr/local/bin/virtualenvwrapper.sh && workon {0}".format(environment_name)):
+        yield
+
+
 def provisioned():
     return False
 
 
+def install_packages():
+    """Installs required OS packages"""
+    sudo("apt-get -y install apache2 libapache2-mod-wsgi")
+    sudo("apt-get -y install postgresql postgresql-client postgresql-server-dev-9.1")
+    sudo("apt-get -y install python-pip python2.7-dev")
+    sudo("apt-get -y install libffi-dev libxml2-dev libxslt-dev")
+
+
+def install_virtualenv():
+    sudo("pip install virtualenv")
+    sudo("pip install virtualenvwrapper")
+
+
+def make_virtualenv(environment):
+    with shell_env(WORKON_HOME='.virtualenvs'):
+        run("source /usr/local/bin/virtualenvwrapper.sh && mkvirtualenv --no-site-packages %s" % environment.VENV)
+
+
 @task
 def provision():
-    pass
+    """ Provisions initial installation"""
+    environment = env.environment
+    install_packages()
+    install_virtualenv()
+    make_virtualenv(environment)
